39937462629629109eef710a250b98b5c9b1b24f
Jawadban <jawadbanga@gmail.com>
2017-05-21 23:47:57 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+
+React.render(
+	<BrowserRouter>
+		<h1>Welcome Jawad</h1>
+	</BrowserRouter>
+	, document.getElementById('root'))
